4a16c43009a22231fb61f17e0dbdb23beb457dd8
Stevemz09 <44810699+Stevemz09@users.noreply.github.com>
2018-11-06 08:25:22 -0700

    Add files via upload

---
+++ Hello world001.py
@@ -0,0 +1,10 @@
+print("Hello World !")
+print("My name is Steve Martinez")
+print("I like to play baseball")
+var = 2018
+print("Today is November the 5th", var)
+print(7+13)
+print(7-13)
+print(7*8)
+print(28/5)
+print(2**3)
