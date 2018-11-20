8828cd55da055e97c622854afaad9e1446282df9
Stevemz09 <44810699+Stevemz09@users.noreply.github.com>
2018-11-20 08:45:28 -0700

    Add files via upload

---
+++ evan_num 2.py
@@ -0,0 +1,16 @@
+
+'''
+Using the keyboard input
+input()
+'''
+
+num = int(input("Enter your integer :"))
+print(num)
+
+if num%2== 0:
+    print("Number :", num, " is Even")
+else :
+    print("Number  :", num, " is ODD")
+
+
+
