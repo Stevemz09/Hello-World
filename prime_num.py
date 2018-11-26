fb8b6c9821014b1cb979626fb23e94efc8190390
Stevemz09 <44810699+Stevemz09@users.noreply.github.com>
2018-11-26 08:47:51 -0700

    Add files via upload

---
+++ prime_num.py
@@ -0,0 +1,17 @@
+'''
+Find whether a given number is PRIME
+using a for loop
+'''
+
+num = int(input("Enter your number :"))
+
+for divisor in range(2, num):
+   print(divisor)
+
+#    if num % divisor == 0:
+ #       print(divisor )
+ #       print( "The number is not prime, it has a divisor: ", divisor)
+ #       exit()
+#print("The number is prime")
+
+#exit()
